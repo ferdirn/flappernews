15152e7bf4fdc40f5cf52c1e16caad26fdc54001
Ferdi Ramdhon <ferdi@moxy.asia>
2015-12-08 16:26:20 +0700

    a wrap

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -2,11 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 var passport = require('passport');
+var jwt = require('express-jwt');
 
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 var User = mongoose.model('User');
+
+var auth = jwt({
+  secret: 'SECRET',
+  userProperty: 'payload'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,8 +28,9 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
   var post = new Post(req.body);
+  post.author = req.payload.username;
 
   post.save(function(err, post) {
     if (err) {
@@ -57,7 +64,7 @@ router.get('/posts/:post', function(req, res) {
   });
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
   req.post.upvote(function(err, post) {
     if (err) {
       return next(err);
@@ -66,9 +73,10 @@ router.put('/posts/:post/upvote', function(req, res, next) {
   });
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
+  comment.author = req.payload.username;
 
   comment.save(function(err, comment) {
     if (err) {
@@ -100,7 +108,7 @@ router.param('comment', function(req, res, next, id) {
   });
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
   req.comment.upvote(function(err, comment) {
     if (err) {
       return next(err);
@@ -131,6 +139,28 @@ router.post('/register', function(req, res, next) {
       token: user.generateJWT()
     });
   });
+});
+
+router.post('/login', function(req, res, next) {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).json({
+      message: 'Please fill out all fields'
+    });
+  }
+
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+
+    if (user) {
+      return res.json({
+        token: user.generateJWT()
+      });
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
 });
 
 module.exports = router;
